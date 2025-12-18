f9b5d7f7fc5933b4763e79409fc16f1671a6141f
Kloshar <kloshar13@yahoo.com>
2025-12-18 09:32:21 +0300

    try contructions added

--- Program.cs
+++ Program.cs
@@ -18,28 +18,28 @@ class Programm
             do
             {
                 Console.WriteLine("\nPlease enter your command:");
-                userCommand = Console.ReadLine();
+                userCommand = Console.ReadLine()!;
                 Console.WriteLine();
-                switch (userCommand.ToUpper())
+                switch (userCommand!.ToUpper())
                 {
-                    //case "I":
-                    //    InsertNewCar(invDaL);
-                    //    break;
-                    //case "U":
-                    //    UpdateCarPetName(invDaL);
-                    //    break;
-                    //case "D":
-                    //    DeleteCar(invDaL);
-                    //    break;
+                    case "I":
+                        InsertNewCar(invDaL);
+                        break;
+                    case "U":
+                        UpdateCarPetName(invDaL);
+                        break;
+                    case "D":
+                        DeleteCar(invDaL);
+                        break;
                     case "L":
                         ListInventory(invDaL);
                         break;
                     case "S":
                         Showinstructions();
                         break;
-                    //case "P":
-                    //    LookUpPetName(invDaL);
-                    //    break;
+                    case "P":
+                        LookUpPetName(invDaL);
+                        break;
                     case "Q":
                         userDone = true;
                         break;
@@ -64,13 +64,92 @@ class Programm
     }
     static void DisplayTable(DataTable dt)
     {
-        for(int c = 0; c < dt.Rows.Count; c++) Console.WriteLine(dt.Columns[c].ColumnName + "\t");
+        for(int c = 0; c < dt.Columns.Count; c++)
+        {
+            Console.Write(dt.Columns[c].ColumnName + "\t");
+        }            
         Console.WriteLine("\n------------------------------------------------------");
+
         for(int r = 0; r < dt.Rows.Count; r++)
         {
-            for(int c = 0; c < dt.Columns.Count; c++) Console.WriteLine(dt.Rows[r][c].ToString() + "\t");
-        }
-        Console.WriteLine();
+            for(int c = 0; c < dt.Columns.Count; c++)
+            {
+                Console.Write(dt.Rows[r][c].ToString() + "\t");
+            }
+            Console.WriteLine();
+        }        
     }
+    static void ListInventoryViaList(InventoryDAL inventoryDAL)
+    {
+        List<NewCar> record = inventoryDAL.GetAllInventoryAsList();
+        foreach (NewCar c in record)
+        {
+            Console.WriteLine($"CarID: {c.CarID}, Make: {c.Make}, Color: {c.Color}, PetName: {c.PetName}");
+        }
+    }
+    static void DeleteCar(InventoryDAL invDAL)
+    {
+        Console.WriteLine("Enter ID of Car to delete: ");        
+        try
+        {
+            int id = int.Parse(Console.ReadLine()!);
+            invDAL.DeleteCar(id);
+        }
+        catch(Exception ex) { Console.WriteLine(ex.Message); }
+    }
+    static void InsertNewCar(InventoryDAL invDAL)
+    {
+        int newCarID = 0;
+        string newCarMake, newCarColor, newCarPetName;
+        
+        while(newCarID.GetType().GetType() != typeof(int) && newCarID == 0)
+        {
+            Console.WriteLine("Enter Car ID:");
+            try { newCarID = int.Parse(Console.ReadLine()!); }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }            
+        }
 
+        Console.WriteLine("Enter Car Make:");
+        newCarMake = Console.ReadLine()!;
+
+        Console.WriteLine("Enter Car Color:");
+        newCarColor = Console.ReadLine()!;
+
+        Console.WriteLine("Enter Car Name:");
+        newCarPetName = Console.ReadLine()!;
+
+        //newCarID = 2;
+        //newCarMake = "Lada";
+        //newCarColor = "Green";
+        //newCarPetName = "Largus";
+
+        try
+        {
+            NewCar c = new NewCar { CarID = newCarID, Make = newCarMake, Color = newCarColor, PetName = newCarPetName };
+            invDAL.InsertAuto(c);
+        }
+        catch(Exception ex) { Console.WriteLine(ex.Message); }
+
+    }
+    static void UpdateCarPetName(InventoryDAL invDAL)
+    {
+        Console.WriteLine("Enter Car ID: ");
+        int CarID = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter New Pet Name: ");
+        string newCarPetName = Console.ReadLine()!;
+        invDAL.UpdateCarPetName(CarID, newCarPetName);
+    }
+    static void LookUpPetName(InventoryDAL invDAL)
+    {
+        Console.WriteLine("Enter ID of car to look up: ");
+        try
+        {
+            int id = int.Parse(Console.ReadLine()!);
+            Console.WriteLine($"Petname of {id} is {invDAL.LookUpPetName(id).TrimEnd()}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
